e092ff438e4da5dc078042eeb63834ec239aa147
heetbeet <you@example.com>
2019-10-22 10:17:41 +0200

    better git customs naming convensions

--- customisations.sh
+++ customisations.sh
@@ -25,16 +25,16 @@ alias wifirestart="nmcli radio wifi off; sleep 7; nmcli radio wifi on"
 #*******************************************************
 # My custom git commands
 #*******************************************************
-alias git-myreset="git stash save --keep-index --include-untracked; git stash drop"
+alias git-unsafe-reset="git stash save --keep-index --include-untracked; git stash drop"
 
-function git-mystash() {
+function git-named-stash() {
     git stash push --include-untracked -m "mystash_$1"
 }
 
-function git-myapply() {
+function git-named-stash-apply() {
     git stash apply $(git stash list | grep "mystash_$1" | cut -d: -f1)
 }
 
-function git-mydrop() {
+function git-names-stash-drop() {
 	git stash drop $(git stash list | grep "mystash_$1" | cut -d: -f1)
 }
